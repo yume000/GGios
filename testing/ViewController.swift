bcc5043e02fd9ccb366e51b4f9ad6b9fae2b52e4
yume <yume@kaihan-macbook-pro.local>
2016-04-30 11:11:40 +0800

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by yume on 2016/4/30.
+//  Copyright © 2016年 yume. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
